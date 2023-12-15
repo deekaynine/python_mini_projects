f0f68b17eb9dd6e76431862365a38cc7de6a5945
matt <matthewtse123work@gmail.com>
2023-12-14 19:09:05 -0500

    flask blog

--- python_flask/personal_site_flask/main.py
+++ python_flask/personal_site_flask/main.py
@@ -1,16 +1,27 @@
 from flask import Flask, render_template
 import requests
+from post import Post
 
-
-
+posts = requests.get("https://api.npoint.io/c790b4d5cab58020d391").json()
+post_objects = []
+for post in posts:
+    post_obj = Post(post["id"], post["title"], post["subtitle"], post["body"])
+    post_objects.append(post_obj)
 app = Flask(__name__)
+
 
 @app.route('/')
 def home():
-    data = requests.get("https://api.npoint.io/c790b4d5cab58020d391")
-    posts = data.json()
-    return render_template("index.html", posts=posts)
+    return render_template("index.html", posts=post_objects)
 
+
+@app.route('/post/<int:post_id>')
+def get_post(post_id):
+    single_post = None
+    for blog_post in post_objects:
+        if blog_post.id == post_id:
+            single_post = post
+    return render_template("post.html", post=single_post)
 
 
 if __name__ == "__main__":

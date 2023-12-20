622d150ba8f9eb18fc538fc3a4f6ac25bab26a7a
matt <matthewtse123work@gmail.com>
2023-12-20 17:02:20 -0500

    flask WTF form validation with flask-boostrap-forms

--- python_flask/WTforms_flask/main.py
+++ python_flask/WTforms_flask/main.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Email, Length
+from flask_bootstrap import Bootstrap5
 
 
 class LoginForm(FlaskForm):
@@ -14,7 +15,7 @@ class LoginForm(FlaskForm):
 
 app = Flask(__name__)
 app.secret_key = "bob123"
-
+bootstrap = Bootstrap5(app)
 
 @app.route("/")
 def home():
